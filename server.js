5599e97a590bae6ccaf854e354d846a90b377f31
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-09-18 23:55:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,35 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/fb-logo.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'fb-logo.png'));
+});
+app.get('/ui/insta-logo.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'insta-logo.jpg'));
+});
+app.get('/ui/twitter-logo.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'twitter-logo.png'));
+});
+app.get('/ui/pic1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pic1.jpg'));
+});
+app.get('/ui/pic2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pic2.jpg'));
+});
+app.get('/ui/pic3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pic3.jpg'));
+});
+app.get('/ui/pic4.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pic4.jpg'));
+});
+app.get('/ui/pic5.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pic5.jpg'));
+});
+app.get('/ui/pic6.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pic6.jpg'));
+});
+app.get('/ui/profilepic.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profilepic.jpg'));
 });
 
 
