8d42689360559dec40f360c10e2e2f254cbc4135
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-02 22:53:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/contact.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
 });
 
-app.get('/post.html', function (req, res) {
+app.get('/post1.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'post.html'));
 });
 
