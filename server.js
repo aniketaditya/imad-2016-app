0a3050be62909c476c014a097560998c11dff99a
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-02 23:07:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,22 @@ app.get('/vendor/bootstrap/fonts/glyphicons-halflings-regular.svg', function (re
   res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/fonts' , 'glyphicons-halflings-regular.svg'));
 });
 
+app.get('/vendor/bootstrap/fonts/glyphicons-halflings-regular.eot', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/fonts' , 'glyphicons-halflings-regular.eot'));
+});
+
+app.get('/vendor/bootstrap/fonts/glyphicons-halflings-regular.ttf', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/fonts' , 'glyphicons-halflings-regular.ttf'));
+});
+
+app.get('/vendor/bootstrap/fonts/glyphicons-halflings-regular.woff', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/fonts' , 'glyphicons-halflings-regular.woff'));
+});
+
+app.get('/vendor/bootstrap/fonts/glyphicons-halflings-regular.woff2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/vendor/bootstrap/fonts' , 'glyphicons-halflings-regular.woff2'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
