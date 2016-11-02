7e33f40e5eebed1bef282335d9cec989969541df
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-02 22:37:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/about.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
 
+app.get('/contact.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
+});
+
+app.get('/post.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'post.html'));
+});
+
+app.get('/css/my-blog.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/css', 'my-blog.css'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
