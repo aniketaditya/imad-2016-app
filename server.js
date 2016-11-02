cd62234fc6ac3d04e0b4505dc156063b5c9f7b4e
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-03 04:39:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ app.get('/index.html', function (req, res) {
 });
 
 app.get('/articles/:articleName',function (req,res) {
-    pool.query("SELECT * FROM article WHERE title = " +req.params.articleName, function(err,result) {
+    pool.query("SELECT * FROM article WHERE title = '" +req.params.articleName +"'", function(err,result) {
         if(err){
             res.status(500).send(err.toString());
     } else {
