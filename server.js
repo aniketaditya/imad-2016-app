99fdba0842d168eec181ae25b961a63a3e17344a
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-03 16:11:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-app.use(express.static(__dirname + '/ui'));
-
 function createTemplate (data) {
     var title = data.title;
     var subtitle = data.subtitle;
