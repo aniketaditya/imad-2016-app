f00d68f51c0275ffe58a21c4fe8fcd34da7dd7fa
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-03 19:22:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -293,6 +293,10 @@ app.get('/vendor/font-awesome/fonts/fontawesome-webfont.woff', function (req, re
   res.sendFile(path.join(__dirname, 'ui/vendor/font-awesome/fonts' , 'fontawesome-webfont.woff2'));
 });
 
+  app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui' , 'main.js'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
