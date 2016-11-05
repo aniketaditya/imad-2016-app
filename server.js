28fa2b4c1dcb534c3cb9c4db5c0c1968dbbd26fc
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-05 18:47:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ function createTemplate (data) {
 
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('/img/technology.jpg')">
+    <header class="intro-header" style="background-image: url('/img/${background-image}')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
