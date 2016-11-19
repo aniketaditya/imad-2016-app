318013a62e14cd4099ba387a83f9a0ce61c8fa04
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-19 17:27:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -137,8 +137,7 @@ function loadArticles () {
                                  </h3>
                                 </a>
                     <p class="post-meta">Posted by <a href="#">${articleData[i].author}</a> on Date</p>
-                </div>
-                <hr>';`
+                </div>';`
                 }
                 articles.innerHTML = content;
             } else {
