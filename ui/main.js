0ce5b4ba7d51e7b77949b4122eeb75cd91b25e25
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-19 16:18:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,16 @@ function loadLoginForm () {
     var loginHtml = `    <h3>Login/Register Here for Posting Comments on the Articles!</h3>
         <div class="row control-group">
             <div class="form-group col-xs-12 floating-label-form-group controls">
-                <label>Userame</label>
+              <label>Userame</label>
                 <input type="text" class="form-control" id="username" placeholder="Enter Your Username" />
-                </div>
-                </div>
-                <div class="row control-group">
+            </div>
+        </div>
+        <div class="row control-group">
             <div class="form-group col-xs-12 floating-label-form-group controls">
-                <label>Password</label>
-        <input type="password" class="form-control" id="password" placeholder="Password" />
+              <label>Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password" />
+            </div>
+        </div>
         <br/>
         <input type="submit" class="btn btn-default" id="login_btn" value="Login" />
         <input type="submit" class="btn btn-default" id="register_btn" value="Register" />
