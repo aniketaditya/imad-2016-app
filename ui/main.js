23952aad032cbf4f3625ecfb0380ba6cd64b5c78
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-19 20:53:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -136,7 +136,7 @@ function loadArticles () {
                                      ${articleData[i].subtitle}
                                  </h3>
                                 </a>
-                    <p class="post-meta">Posted by <a href="#">${articleData[i].author}</a> on ${articleData[i].date.toJASON()}</p>
+                    <p class="post-meta">Posted by <a href="#">${articleData[i].author}</a> on ${articleData[i].date.toString()}</p>
                 </div>
                 </div>
                 </div>`;
