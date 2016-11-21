799cc93928f079a6aa40b29f450afaca5c7cd494
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-21 05:49:03 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -2,11 +2,10 @@ var currentArticleTitle = window.location.pathname.split('/')[1];
 
 function loadCommentForm () {
     var commentFormHtml = `
-        <h5>Submit a comment</h5>
         <div class="row control-group">
             <div class="form-group col-xs-12 floating-label-form-group controls">
               <label>Write your Comment</label>
-        <textarea id="comment_text" rows="4" cols="80" placeholder="Enter your comment here..." required="required"></textarea>
+        <textarea id="comment_text" rows="4" cols="80" placeholder="Enter your comment here..." required data-validation-required-message="Please write some comments></textarea>
         </div>
         </div>
         <br/>
