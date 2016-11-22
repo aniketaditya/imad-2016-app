4cef778c6798f30d841368e3ba88854295de5f88
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-23 00:35:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 function loadLoginForm () {
          var loginHtml = ` <h3>Login/Register Here for Posting Comments on the Articles!</h3>
         <div class="row control-group">
-        <form>
             <div class="form-group col-xs-12 floating-label-form-group controls">
               <label>Username</label>
                 <input type="text" class="form-control" id="username" placeholder="Enter Your Username" required>
@@ -16,7 +15,6 @@ function loadLoginForm () {
         <br/>
         <input type="submit" class="btn btn-default" id="login_btn" value="Login" />
         <input type="submit" class="btn btn-default" id="register_btn" value="Register" />
-        </form>
         `;
              
         
@@ -51,6 +49,10 @@ function loadLoginForm () {
         // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
+        if (username === '' || password === '') {
+        alert("Username/Password field can't be left empty");
+        return;
+    }
         request.open('POST', '/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
@@ -80,8 +82,10 @@ function loadLoginForm () {
         // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
+     if (username === '' || password === '') {
+        alert("Username/Password field can't be left empty");
+        return;
+    }
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
