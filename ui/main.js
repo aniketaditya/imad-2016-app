d5253d802d83a42d855f4841818335900a5346be
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-24 00:09:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -105,7 +105,7 @@ function escapeHTML (text)
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
-        <h3> Hi!</h3> <p style="color:#ff0282";font-weight:bold>${username.toUpperCase()}</p>
+        <h3> Hi!</h3> <p style="color:#ff0282";font-weight:bold>${escapeHTML(username).toUpperCase()}</p>
         <a href="/logout"><input type="submit" class="btn btn-default" id="logout" value="Logout" /></a>
     `;
 }
