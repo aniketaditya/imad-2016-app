fbd3ba9b9ee4ced9fa67786947d0f8094d84e4c3
Aniket Aditya <mrcool.aniketaditya@gmail.com>
2016-11-24 02:13:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -279,7 +279,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.status(200).redirect('/?msg=session%20destroyed');
+   res.status(200).redirect('/?msg=logged%20out');
 });
 
 var pool = new Pool(config);
